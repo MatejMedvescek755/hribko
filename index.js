b314b5d75088cd5819a866b818ceb4a90db9187a
MatejMedvescek755 <48921892+MatejMedvescek755@users.noreply.github.com>
2026-07-19 13:36:42 +0200

    add height and lhribi.net link to the output message

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cheerio = require("cheerio");
 const path = require("path");
 const nodeHtmlToImage = require('node-html-to-image')
 
-
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [
@@ -13,12 +12,16 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-//https://discord.com/oauth2/authorize?client_id=1528135657132654742&permissions=8584986789675007&scope=bot+applications.commands
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 client.login(process.env.DISCORD_TOKEN);
 // Documentation: https://discordjs.guide/legacy/app-creation/main-file
+// Invite url: https://discord.com/oauth2/authorize?client_id=1528135657132654742&permissions=8584986789675007&scope=bot+applications.commands
+
+const hribUrl = (hribName, gorovjeId, hribId) => `https://www.hribi.net/gora/${hribName}/${gorovjeId}/${hribId}`;
+const hribSearch = (query) => `https://www.hribi.net/iskalnik.asp?q=${query}`;
+const hribVreme = (hribName, gorovjeId, hribId) => `https://www.hribi.net/vreme_gora/${hribName}/${gorovjeId}/${hribId}`;
 
 client.on("messageCreate", async (message) => {
   try {
@@ -30,17 +33,16 @@ client.on("messageCreate", async (message) => {
     message.channel.sendTyping();
 
     const { gorovjeId, hribId, hribName, alternativeResults } =
-      await getMountainId(hrib);
+      await getMountainId(hrib);  
 
-    // const vremeUrl = `https://www.hribi.net/vreme_hribi_net/000000/250/${gorovjeId}/5/${hribId}`;
-    // const image = await renderWeather(vremeUrl);
+    let msg = `Vreme za [${hribName}](<${hribUrl(hribName, gorovjeId, hribId)}>)`;
+    const visina = await getVisina(hribName, gorovjeId, hribId);
+    if (visina !== undefined) msg += ` (${visina})`
 
-    let msg = "Vreme za " + hribName;
     if (alternativeResults.length > 0) {
-      msg += "\n" + "Poskusi tudi: " + alternativeResults.join(", ");
+      msg += "\n" + "(Poskusi tudi: " + alternativeResults.join(", ") + ")";
     }
 
-    //https://www.hribi.net/vreme_gora/triglav/1/1
     const table = await parseTableWeek(hribName, gorovjeId, hribId);
     const imagePath = path.join(__dirname, "weather.png");
 
@@ -82,8 +84,7 @@ client.on("messageCreate", async (message) => {
 });
 
 const getMountainId = async (query) => {
-  const url = `https://www.hribi.net/iskalnik.asp?q=${query}`;
-  const response = await fetch(url);
+  const response = await fetch(hribSearch(query));
   const html = await response.text();
 
   const $ = cheerio.load(html);
@@ -113,23 +114,8 @@ const getMountainId = async (query) => {
   }
 };
 
-async function parseTableDay(url) {
-  const response = fetch(url);
-  const $ = cheerio.load(await (await response).text());
-  const items = $("#pan1 div table tbody");
-  const htmlItems = [];
-  for (let i = 0; i < items.length - 1; i += 2) {
-    const first = $.html(items[i]);
-    const second = items[i + 1] ? $.html(items[i + 1]) : "";
-
-    htmlItems.push(`<div>${first + second}</div>`);
-  }
-  return htmlItems;
-}
-
 async function parseTableWeek(hribName, gorovjeId, hribId) {
-  const url = `https://www.hribi.net/vreme_gora/${hribName}/${gorovjeId}/${hribId}`;
-  const response = await fetch(url);
+  const response = await fetch(hribVreme(hribName, gorovjeId, hribId));
 
   if (!response.ok) {
     throw new Error(`Zahteva propadla: ${response.status}`);
@@ -153,4 +139,19 @@ async function parseTableWeek(hribName, gorovjeId, hribId) {
   });
 
   return `<table>${tbody.html()}</table>`;
+}
+
+const getVisina = async (hribName, gorovjeId, hribId) =>{
+    try {
+      const url = hribUrl(hribName, gorovjeId, hribId);
+      const response = await fetch(url);
+      const html = await response.text();
+      const $ = cheerio.load(html);
+      const visina = $('b').filter((_, el)=>$(el).text().trim() == "Višina:").get(0).nextSibling.nodeValue.trim();
+      return visina
+    } catch(error) {
+      console.error(error);
+      console.error("ni višine :(");
+      return undefined;
+    }
 }
